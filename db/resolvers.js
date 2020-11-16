2cf7e4bbdb15d7e94da2f1a8aaa18e97f713fd64
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-16 13:40:10 -0600

    Guardando usuario en la base de datos

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,12 +1,30 @@
+const Usuario = require("../models/Usuario");
+
 // Resolvers
 const resolvers = {
   Query: {
     obtenerCurso: () => "Creando nuevo usuario",
   },
   Mutation: {
-    nuevoUsuario: (_, { input }) => {
-      console.log(input);
-      return "Creando nuevo usuario";
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // Revisar si el usuario ya está registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error("El usuario ya está registrado");
+      }
+
+      // Hashear el password
+
+      // Guardarlo en la base de datos
+      try {
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
