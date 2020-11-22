adfd2df573855d960c092fa33df2945256eccbdc
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-22 17:35:36 -0600

    Obtener mejores vendedores y ordenarlos de mayor a menor

--- db/resolvers.js
+++ db/resolvers.js
@@ -119,10 +119,39 @@ const resolvers = {
           },
         },
         {
+          $limit: 10,
+        },
+        {
           $sort: { total: -1 },
         },
       ]);
       return clientes;
+    },
+    mejoresVendedores: async () => {
+      const vendedores = await Pedido.aggregate([
+        { $match: { estado: "COMPLETADO" } },
+        {
+          $group: {
+            _id: "$vendedor",
+            total: { $sum: "$total" },
+          },
+        },
+        {
+          $lookup: {
+            from: "usuarios",
+            localField: "_id",
+            foreignField: "_id",
+            as: "vendedor",
+          },
+        },
+        {
+          $limit: 3,
+        },
+        {
+          $sort: { total: -1 },
+        },
+      ]);
+      return vendedores;
     },
   },
   Mutation: {
