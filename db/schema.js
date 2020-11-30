2671bef4456d93c098eae9cd68fbf70a222f3dc5
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 18:55:14 -0600

    Mostrando pedidos

--- db/schema.js
+++ db/schema.js
@@ -45,6 +45,7 @@ const typeDefs = gql`
   type PedidoGrupo {
     id: ID
     cantidad: Int
+    nombre: String
   }
 
   type TopCliente {
@@ -86,6 +87,8 @@ const typeDefs = gql`
   input PedidoProductoInput {
     id: ID
     cantidad: Int
+    nombre: String
+    precio: Float
   }
 
   input PedidoInput {
